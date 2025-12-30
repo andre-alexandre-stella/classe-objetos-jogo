09e220828e20d60775d963ffc50ae60172d0527e
andre-alexandre-stella <andre.alexandre.stella@gmail.com>
2025-12-30 17:37:20 -0300

    criada a classe 'Heroi'

---
+++ app.js
@@ -0,0 +1,30 @@
+class Heroi{
+    constructor(nomeHeroi, idadeHeroi, tipoHeroi){
+        this.nomeHeroi = nomeHeroi;
+        this.idadeHeroi = idadeHeroi;
+        this.tipoHeroi = tipoHeroi;
+        let tipoAtaque = '';
+        switch(this.tipoHeroi){
+            case('mago'):
+                tipoAtaque = 'magia';
+                break;
+            case('guerreiro'):
+                tipoAtaque = 'espada';
+                break;
+            case('monge'):
+                tipoAtaque = 'artes marciais';
+                break;
+            case('ninja'):
+                tipoAtaque = 'shuriken';
+                break;
+            default:
+                tipoAtaque = 'Tipo de herói inexistente!';
+        }
+        this.tipoAtaque = tipoAtaque;
+    }
+
+    atacar(){
+        let mensagem = `O ${this.tipoHeroi} atacou usando ${this.tipoAtaque}.`;
+        console.log(mensagem);
+    }
+}
